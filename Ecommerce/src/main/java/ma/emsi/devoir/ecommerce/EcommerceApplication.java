cafb2eaa808130fae3084a5894c227edea5890ae
Abdelaali <Abdelaali@192.168.1.117>
2021-01-10 00:23:59 +0100

    insertion d'un article

--- Ecommerce/src/main/java/ma/emsi/devoir/ecommerce/EcommerceApplication.java
+++ Ecommerce/src/main/java/ma/emsi/devoir/ecommerce/EcommerceApplication.java
@@ -16,7 +16,7 @@ public class EcommerceApplication {
 	ApplicationContext	applicationContext = SpringApplication.run(EcommerceApplication.class, args);
 		
 	IArticleService iArticleService= applicationContext.getBean(IArticleService.class);	
-	ArticleVO articleVO= ArticleVO.builder().reference("TestReference4").price(10).designation("TestDesignation4").build();
+	ArticleVO articleVO= ArticleVO.builder().reference("TestReference7").price(10).designation("TestDesignation7").build();
 	iArticleService.saveOrUpdate(articleVO);
 	ArrayList<ArticleVO> artList = (ArrayList<ArticleVO>) iArticleService.findAll();
 	artList.forEach(art -> System.err.println("Ref: "+art.getReference()+" Price: "+art.getPrice()+" Design: "+art.getDesignation()));
